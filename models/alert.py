1b7971c4e74b609f9ebe874a5712db9c135ebca0
Alexander1231 <baryan.acb@gmail.com>
2019-07-04 13:16:54 -0500

    home

---
+++ models/alert.py
@@ -0,0 +1,31 @@
+import uuid
+from typing import Dict
+from models.item import Item
+from models.model import Model
+
+
+class Alert(Model):
+    collection = "alerts"
+
+    def __init__(self, item_id: str, price_limit: float, _id: str = None):
+        super().__init__()
+        self.item_id = item_id
+        self.item = Item.get_by_id(item_id)
+        self.price_limit = price_limit
+
+        self._id = _id or uuid.uuid4().hex
+
+    # def json(self) -> Dict:
+    #     return {
+    #         "_id": self._id,
+    #         "price_limit": self.price_limit,
+    #         "item_id": self.item_id
+    #     }
+
+    def load_item_price(self) -> float:
+        self.item.load_price()
+        return self.item.price
+
+    def notify_if_price_reached(self):
+        if self.item.price < self.price_limit:
+            print("Item %s has reached a price unde %s. Lastest price: %s." % (self.item, self.price_limit, self.item.price))
